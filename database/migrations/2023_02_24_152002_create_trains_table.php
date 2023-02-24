081a63f65d1a53bbd6a818365efdeed45a4fa49c
EsterSPR <web.esterspreafico@gmail.com>
2023-02-24 18:06:55 +0100

    create colonne

--- database/migrations/2023_02_24_152002_create_trains_table.php
+++ database/migrations/2023_02_24_152002_create_trains_table.php
@@ -15,7 +15,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string();
+            $table->string('azienda', 30);
+            $table->string('stazione_partenza', 50);
+            $table->string('stazione_arrivo', 50);
+            $table->time('partenza', $precision = 0);
+            $table->time('arrivo', $precision = 0);
+            $table->string('codice', 20);
+            $table->smallInteger('carrozze');
+            $table->boolean('in_orario')->unsigned();
+            $table->boolean('cancellato')->unsigned();
+            $table->timestamps();
         });
     }
 
